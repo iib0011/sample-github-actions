e2d6073d2bddd073fbae602b0d22e4c6106bf413
Ibrahima <ibracool99@gmail.com>
2022-02-18 11:40:29 +0100

    example commit

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+export const marginStyle = {
+    display: "block",
+    marginBottom: 32
+};
+export const btnDefaultStyle = {
+    padding: "16px 32px",
+    cursor: "pointer",
+    fontSize: 16,
+    color: "white",
+    border: "none",
+    borderRadius: "40px",
+    outline: "none",
+    width: 300
+};
+
